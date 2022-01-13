d16bdff7bc90bc092eda82dfae7e393d344aa2da
Neeraj Gupta <neerajfullstack@gmail.com>
2022-01-13 10:06:03 +0530

    timer code done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('It works')
+class Timer {
+  constructor(durationInput, startButton, pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    // add event listneres to dom elements passed into the class.
+    this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
+  }
+
+  start = () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  };
+
+  tick = () => {
+    this.timeRemaining -= 1;
+  };
+
+  // eslint-disable-next-line class-methods-use-this
+  pause = () => {
+    clearInterval(this.interval);
+  };
+
+  get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+  }
+
+  set timeRemaining(time) {
+    this.durationInput.value = time;
+  }
+}
+
+const duration = document.querySelector('#duration');
+const start = document.querySelector('#start');
+const pause = document.querySelector('#pause');
+
+const timer = new Timer(duration, start, pause);
